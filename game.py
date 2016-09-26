c1d504b23a6c01177155f72fe89ba8f5bda29045
kavyamohan <kavya2_mohan@yahoo.in>
2016-09-26 06:54:12 +0530

    first

---
+++ game.py
@@ -0,0 +1,44 @@
+import  msvcrt
+import time
+finish=10
+count=0
+a=0
+print"press enter key to get started"
+raw_input()
+s_time=time.time()
+while(1):
+	key=msvcrt.getch()
+	if key=='\xe0':
+		count=count+1
+		print"d",
+
+		if count==finish:
+			break
+print"go down"
+		
+finish=10
+count=0
+while(1):
+	key=msvcrt.getch()
+	if key=='\xe0':
+		count=count+1
+		print"\t\t\ts"
+		if count==finish:
+			break
+print"\t\t\tgo right"
+finish=10
+count=0
+while(1):
+	key=msvcrt.getch()
+	if key=='\xe0':
+		count=count+1
+		if a==0:
+			print"\t\t\t",
+			a=1
+		print"d",
+		if count==finish:
+			break
+time_elasped=time.time()-s_time
+
+print"\ncongrats you hav finished" 
+print "\nthe time taken %s"%str(time_elasped)
